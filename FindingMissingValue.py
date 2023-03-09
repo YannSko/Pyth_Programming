876125985e3a444454dd2d466e298d11f096cd15
YannSko <101813005+YannSko@users.noreply.github.com>
2023-03-09 18:13:31 +0100

    Create FindingMissingValue.py

---
+++ FindingMissingValue.py
@@ -0,0 +1,12 @@
+#Given an array containing a range of numbers from 0 to n with a missing number, find the missing number in the input array.
+def findMissingNumbers(n):
+    numbers = set(n)
+    length = len(n)
+    output = []
+    for i in range(1, n[-1]):
+        if i not in numbers:
+            output.append(i)
+    return output
+    
+listOfNumbers = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16]
+print(findMissingNumbers(listOfNumbers))
