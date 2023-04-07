490cf74eda1ff0313d7ee66cdd7171ad9af2a527
YannSko <101813005+YannSko@users.noreply.github.com>
2023-04-07 15:35:57 +0200

    bot

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+intents = discord.Intents.all()
+
+client = commands.Bot(command_prefix="!", intents = intents)
+
+
+
+@client.event
+async def on_message(message):
+    if (message.author == client.user):
+        return
+   
+
+    if message.content.startswith("Hello"):
+        await message.channel.send("hello")
+@client.event
+async def on_ready():
+    channel = client.get_channel(1091265494771843095)
+    await channel.send('the bot is ready')
+    
+
+client.run("MTA5MTI2Mjc0NjI1MjgwNDEyNg.Gsebrp.cJdD2YSX_V6-VC_RuX39NwIxK8PD9INRiq2eSM")
