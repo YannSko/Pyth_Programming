0fba640a851fff33b40e52ecfcc96fc30869d368
YannSko <101813005+YannSko@users.noreply.github.com>
2023-04-23 16:46:27 +0200

    y

---
+++ hash_table.py
@@ -0,0 +1,10 @@
+class Hashtable:
+    def __init__(self,elements):
+        self.bucket_size = len(elements)
+        self.bucket = [[]for i in range [self.bucket] ]
+        self._assign_buckets(elements)
+    
+    def _assign_buckets(self,input_key):
+       
+        
+    def 
