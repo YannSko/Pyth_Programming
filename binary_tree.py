d4d7724246018233d0b4b9035d0e44ad160b47cd
YannSko <101813005+YannSko@users.noreply.github.com>
2023-05-14 13:13:47 +0200

    Update binary_tree.py

--- binary_tree.py
+++ binary_tree.py
@@ -1,60 +1,59 @@
-class node : 
-  def __init__(self,data):
-    self.data = data
-    self.right_node = None
-    self.left_node = None
-    self.responses = []
+class Node:
+    def __init__(self, data, responses=None):
+        self.data = data
+        self.left_node = None
+        self.right_node = None
+        self.responses = responses or []
 
-  def append(self,data,responses):
-    if data < self.data :
-      if self.left_node == None:
-        self.left_node = node(data)
-        self.left_node.responses = responses
-      else:
-        self.left_node.append(data,responses)
-    elif data > self.data:
-      if self.right_node == None:
-        self.right_node = node(data)
-        self.right_node.responses = responses
-      else:
-        self.right_node.append(data,responses)
+    def append(self, data, responses=None):
+        if data < self.data:
+            if self.left_node is None:
+                self.left_node = Node(data, responses)
+            else:
+                self.left_node.append(data, responses)
+        elif data > self.data:
+            if self.right_node is None:
+                self.right_node = Node(data, responses)
+            else:
+                self.right_node.append(data, responses)
 
-  def search(self, data):
-    if data == self.data:
-      return True
-    elif data < self.data:
-      if self.left_node == None:
-        return False
-      else:
-        return self.left_node.search(data)
-    else:
-      if self.right_node == None:
-        return False
-      else:
-        return self.right_node.search(data)
+    def search(self, data):
+        if data == self.data:
+            return self
+        elif data < self.data:
+            if self.left_node is None:
+                return None
+            else:
+                return self.left_node.search(data)
+        else:
+            if self.right_node is None:
+                return None
+            else:
+                return self.right_node.search(data)
 
-  def __str__(self):
-    txt = str(self.data)
-    if self.left_node != None:
-      txt += "-" +str(self.left_node)
-    if self.right_node != None:
-      txt += "-" +str(self.right_node)
-    return txt
+    def __str__(self):
+        txt = str(self.data)
+        if self.left_node is not None:
+            txt += "-" + str(self.left_node)
+        if self.right_node is not None:
+            txt += "-" + str(self.right_node)
+        return txt
 
-  def get_responses(self):
-    return self.responses
-class binary_tree:
-  def __init__(self,data):
-    self.first_node = node(data)
-  
-  def append(self,data):
-    self.first_node.append(data)
+    def get_responses(self):
+        return self.responses
 
-  def search(self,data):
-    return self.first_node.search(data)
 
-  def __str__(self):
-    return str(self.first_node)
-    
+class BinaryTree:
+    def __init__(self, data, responses=None):
+        self.first_node = Node(data, responses)
+
+    def append(self, data, responses=None):
+        self.first_node.append(data, responses)
+
+    def search(self, data):
+        return self.first_node.search(data)
+
+    def __str__(self):
+        return str(self.first_node)
 
     
